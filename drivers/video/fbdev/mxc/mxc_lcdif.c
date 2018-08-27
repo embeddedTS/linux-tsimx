dbeb22acb167af3f6f7559bb9435d5c58cb8f0af
Mark Featherston <mark@embeddedarm.com>
2018-08-27 09:18:56 -0700

    Add okaya and microtips timing

--- drivers/video/fbdev/mxc/mxc_lcdif.c
+++ drivers/video/fbdev/mxc/mxc_lcdif.c
@@ -48,6 +48,16 @@ static struct fb_videomode lcdif_modedb[] = {
 	FB_SYNC_CLK_LAT_FALL,
 	FB_VMODE_NONINTERLACED,
 	0,},
+	{
+	"OKAYA-WVGA", 60, 800, 480, 30066, 50, 70, 2, 2, 50, 50,
+	0,
+	FB_VMODE_NONINTERLACED,
+	0,},
+	{
+	"MICROTIPS-WVGA", 60, 800, 480, 30030, 46, 210, 23, 22, 1, 1,
+	0,
+	FB_VMODE_NONINTERLACED,
+	0,},
 };
 static int lcdif_modedb_sz = ARRAY_SIZE(lcdif_modedb);
 
